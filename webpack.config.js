4bb2dbf193307694b7d4e326f00393ee6d6927ba
AslDeveloper07 <aslwexking777@gmail.com>
2025-05-20 12:27:50 +0500

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,40 @@
-const path = require("path"); //node js
-const htmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path'); //node js
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   //mode
-  mode: "development", //production
+  mode: 'development', //production
   //entry
-  entry: path.resolve(__dirname, "./src/app.js"),
+  entry: {
+    app:path.resolve(__dirname, './src/app.js'),
+    about: path.resolve(__dirname, './src/about.js')
+  },
   //output
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js',
   },
   //plugins
   plugins: [
-    new HtmlWebpackPlugin({  
+    new htmlWebpackPlugin({
       //title,
       title: 'webpack 5😀',
       //filename,
       filename: 'index.html',
       //template
       template: './src/temp.html',
+      //chunks
+      chunks:['app'],
+    }),
+    new htmlWebpackPlugin({
+      //title,
+      title: 'About page',
+      //filename,
+      filename: 'about.html',
+      //template
+      template: './src/tempAbout.html',
+      //chunks
+      chunks:['about'],
     }),
   ],
   //loaders
