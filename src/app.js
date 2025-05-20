c8a7fdf1c9cdbbf9d515969f4b7f645974c0e7ce
AslDeveloper07 <aslwexking777@gmail.com>
2025-05-20 12:51:19 +0500

    add

--- src/app.js
+++ src/app.js
@@ -6,3 +6,4 @@ import { getData, numbers } from "./request";
 getData("Asilbek");
 console.log(numbers);
 console.log("App.js");
+console.log("web developer");
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
-const path = require('path'); //node js
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path"); //node js
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   //mode
-  mode: 'development', //production
+  mode: "development", //production
   //entry
   entry: {
-    app:path.resolve(__dirname, './src/app.js'),
-    about: path.resolve(__dirname, './src/about.js')
+    app: path.resolve(__dirname, "./src/app.js"),
+    about: path.resolve(__dirname, "./src/about.js"),
   },
   //output
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "public"),
+    filename: "[name].js",
   },
   //plugins
   plugins: [
     new htmlWebpackPlugin({
       //title,
-      title: 'webpack 5😀',
+      title: "webpack 5😀",
       //filename,
-      filename: 'index.html',
+      filename: "index.html",
       //template
-      template: './src/temp.html',
+      template: "./src/temp.html",
       //chunks
-      chunks:['app'],
+      chunks: ["app"],
     }),
     new htmlWebpackPlugin({
       //title,
-      title: 'About page',
+      title: "About page",
       //filename,
-      filename: 'about.html',
+      filename: "about.html",
       //template
-      template: './src/tempAbout.html',
+      template: "./src/tempAbout.html",
       //chunks
-      chunks:['about'],
+      chunks: ["about"],
     }),
   ],
   //loaders
@@ -52,5 +52,16 @@ module.exports = {
         ],
       },
     ],
+  },
+  //devServer
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+    port: 5000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
 };
